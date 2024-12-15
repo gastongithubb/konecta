9751d7413e50a9458580895827fa3c4d2e589165
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-12-15 03:13:24 -0300

    Quick commit

--- app/reset-password/[token]/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/reset-password/[token]/page.tsx
-import ResetPassword from '@/components/generales/ResetPassword';
-
-export default function ResetPasswordPage({ params }: { params: { token: string } }) {
-  return <ResetPassword token={params.token} />;
-}
---
+++ app/reset-password/page.tsx
@@ -0,0 +1,6 @@
+// app/reset-password/[token]/page.tsx
+import ResetPassword from '@/components/generales/ResetPassword';
+
+export default function ResetPasswordPage() {
+  return <ResetPassword />;
+}
